5d7dfe9d9e4631d89e9b138364446da193402691
Fernando Prange <fpmenechini@gmail.com>
2026-06-30 00:07:47 -0300

    Adicionado o fish Wezterm movido para .config/wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,8 +9,8 @@ config.font_size = 10
 config.color_scheme = 'Dracula (Official)'
 
 
-config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.97
+config.window_decorations = 'NONE'
+config.window_background_opacity = 0.95
 
 config.enable_tab_bar = false
 
